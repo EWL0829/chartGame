52a9ff62696d2931f245a75b0786a7ff51e8b0b6
liyue <li.yue@okcoin.com>
2019-08-08 17:11:24 +0800

    统一横纵坐标的绘制范围

--- src/component/Chart/index.js
+++ src/component/Chart/index.js
@@ -1,12 +1,132 @@
 import React, { Component } from 'react';
+import * as d3 from 'd3';
 import './index.less';
 
 export default class extends Component {
+    state = {
+        margin: {
+            top: 20,
+            right: 30,
+            bottom: 30,
+            left: 40,
+        },
+        width: 960,
+        height: 500,
+        data: [],
+    };
+
+    componentDidMount() {
+        const { chartData } = this.props;
+        const { lineData } = chartData;
+
+        this.setState({
+            data: lineData
+        }, () => {
+            this.drawChart(lineData);
+        });
+    }
+
+    componentWillReceiveProps(nextProps) {
+        const { chartData } = this.props;
+        const { lineData } = chartData;
+        const newLineData = nextProps.chartData.lineData;
+
+        const oldDataLen = lineData.length;
+        const newDataLen = newLineData.length;
+
+        if (newDataLen !== oldDataLen) {
+            this.setState({
+                data: newLineData
+            }, () => {
+                this.updateChart(newLineData);
+            });
+        }
+    }
+
+    drawChart = (data) => {
+        const { margin, width, height } = this.state;
+
+        const svg = d3.select('#mainChart').append('svg')
+            .attr('class', 'svg-wrap')
+            .attr('width', width)
+            .attr('height', height);
+
+        const x = d3.scaleTime()
+            .domain(d3.extent(this.props.axisRangeData, d => d.date))
+            .range([margin.left, width - margin.right]);
+
+        const xAxis = g => g
+            .attr("transform", `translate(0,${height - margin.bottom})`)
+            .call(d3.axisBottom(x).ticks(width / 80).tickSizeOuter(0));
+
+        const y = d3.scaleLinear()
+            .domain([0, d3.max(this.props.axisRangeData, d => d.value)]).nice()
+            .range([height - margin.bottom, margin.top]);
+
+        const yAxis = g => g
+            .attr("transform", `translate(${margin.left},0)`)
+            .call(d3.axisLeft(y))
+            .call(g => g.select(".tick:last-of-type text").clone()
+                .attr("x", 3)
+                .attr("text-anchor", "start")
+                .attr("font-weight", "bold")
+                .text(data.y));
+
+        const line = d3.line()
+            .defined(d => !isNaN(d.value))
+            .x(d => x(d.date))
+            .y(d => y(d.value));
+
+        // 添加横轴
+        svg.append('g')
+            .call(xAxis);
+
+        // 添加纵轴
+        svg.append("g")
+            .call(yAxis);
+
+        svg.append("path")
+            .attr('class', 'path-line')
+            .datum(data)
+            .attr("fill", "none")
+            .attr("stroke", "steelblue")
+            .attr("stroke-width", 1.5)
+            .attr("stroke-linejoin", "round")
+            .attr("stroke-linecap", "round")
+            .attr("d", line);
+    };
+
+    updateChart = (data) => {
+        const { margin, width, height } = this.state;
+
+        const x = d3.scaleTime()
+            .domain(d3.extent(this.props.axisRangeData, d => d.date))
+            .range([margin.left, width - margin.right]);
+
+        const y = d3.scaleLinear()
+            .domain([0, d3.max(this.props.axisRangeData, d => d.value)]).nice()
+            .range([height - margin.bottom, margin.top]);
+
+        const line = d3.line()
+            .defined(d => !isNaN(d.value))
+            .x(d => x(d.date))
+            .y(d => y(d.value));
+
+        d3.select('.svg-wrap')
+            .append('path')
+            .attr('class', 'new-path-line')
+            .datum(data)
+            .attr("fill", "none")
+            .attr("stroke", "steelblue")
+            .attr("stroke-width", 1.5)
+            .attr("stroke-linejoin", "round")
+            .attr("stroke-linecap", "round")
+            .attr("d", line);
+    };
+
     render() {
         return (
-            <div className="chart-wrap" id="mainChart">
-                {this.props.chartData}
-            </div>
+            <div className="chart-wrap" id="mainChart"></div>
         );
     }
 }
--- src/pages/futureGuide/index.js
+++ src/pages/futureGuide/index.js
@@ -7,10 +7,53 @@ import './index.less';
 export default class extends Component {
     state = {
         step: 0,
+        axisRangeData: [
+            { date: new Date('2019-09-01T00:00'), value: 0 },
+            { date: new Date('2019-09-02T00:00'), value: 10000 },
+            { date: new Date('2019-09-03T00:00'), value: 10000 },
+            { date: new Date('2019-09-04T00:00'), value: 10000 },
+            { date: new Date('2019-09-05T00:00'), value: 10000 },
+            { date: new Date('2019-09-06T00:00'), value: 10000 },
+            { date: new Date('2019-09-07T00:00'), value: 10000 },
+            { date: new Date('2019-09-08T00:00'), value: 10000 },
+            { date: new Date('2019-09-09T00:00'), value: 10000 },
+            { date: new Date('2019-09-10T00:00'), value: 10000 },
+            { date: new Date('2019-09-11T00:00'), value: 10000 },
+            { date: new Date('2019-09-12T00:00'), value: 10000 },
+            { date: new Date('2019-09-13T00:00'), value: 10000 },
+            { date: new Date('2019-09-14T00:00'), value: 10000 },
+            { date: new Date('2019-09-15T00:00'), value: 10000 },
+            { date: new Date('2019-09-16T00:00'), value: 10000 },
+            { date: new Date('2019-09-17T00:00'), value: 10000 },
+            { date: new Date('2019-09-18T00:00'), value: 10000 },
+            { date: new Date('2019-09-19T00:00'), value: 10000 },
+            { date: new Date('2019-09-20T00:00'), value: 10000 },
+        ],
         data: {
             chartData: [
-                1,
-                2,
+                {
+                    lineData: [
+                        { date: new Date('2019-09-01T00:00'), value: 1200 },
+                        { date: new Date('2019-09-02T00:00'), value: 1800 },
+                        { date: new Date('2019-09-03T00:00'), value: 1900 },
+                        { date: new Date('2019-09-04T00:00'), value: 3200 },
+                        { date: new Date('2019-09-05T00:00'), value: 7800 },
+                        { date: new Date('2019-09-06T00:00'), value: 1700 },
+                        { date: new Date('2019-09-07T00:00'), value: 2310 },
+                        { date: new Date('2019-09-08T00:00'), value: 6500 },
+                        { date: new Date('2019-09-09T00:00'), value: 5120 },
+                    ],
+                },
+                {
+                    lineData: [
+                        { date: new Date('2019-09-09T00:00'), value: 5120 },
+                        { date: new Date('2019-09-10T00:00'), value: 6090 },
+                        { date: new Date('2019-09-11T00:00'), value: 1342 },
+                        { date: new Date('2019-09-12T00:00'), value: 5401 },
+                        { date: new Date('2019-09-13T00:00'), value: 7623 },
+                        { date: new Date('2019-09-14T00:00'), value: 8804 },
+                    ],
+                },
                 3,
                 4,
                 5,
@@ -72,11 +115,11 @@ export default class extends Component {
     };
 
     render() {
-        const { step, data } = this.state;
+        const { step, data, axisRangeData } = this.state;
 
         return (
             <div className="future-guide-wrap guide-wrap">
-                <Chart chartData={data.chartData[step]} />
+                <Chart chartData={data.chartData[step]} axisRangeData={axisRangeData} />
                 <Operation operationData={data.operationData[step]}/>
                 <div className="back-prev" onClick={() => this.stepBackward()}>
                     <span className="back-prev-text">&lt;&lt;我后悔了</span>
