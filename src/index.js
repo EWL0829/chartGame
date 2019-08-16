30067e39b2ccbe045a21650f99eda0ba498e4aa8
liyue <li.yue@okcoin.com>
2019-08-16 17:07:14 +0800

    初始化永续合约路线

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route,  } from 'react-router-dom';
 import OptionGuide from './pages/optionGuide';
+import FutureGuide from './pages/futureGuide';
 import './index.less';
 
 class App extends Component {
     render() {
         return (
             <Router>
-                <Route path="/futureGuide" exact component={OptionGuide} />
+                <Route path="/optionGuide" exact component={OptionGuide} />
+                <Route path="/futureGuide" exact component={FutureGuide} />
             </Router>
         )
     }
